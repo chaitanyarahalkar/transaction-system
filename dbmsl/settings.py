b5e0c5c001296ba0b4a930ba12730ec954c25aa5
chaitanyarahalkar <chaitanyarahalkar4@gmail.com>
2018-10-07 16:39:06 +0530

    Added email feature

--- dbmsl/settings.py
+++ dbmsl/settings.py
@@ -134,7 +134,7 @@ LOGIN_REDIRECT_URL = '/otp/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'chaitanyarahalkar2@gmail.com'
